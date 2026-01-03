0bfad4b32047a4e7ee9406341c73dc9c66859fec
Andrew Jeremiah Herrrada Villarroel <andrew.herrada.villarroel@gmail.com>
2026-01-03 16:47:23 -0400

    Config API_KEY

--- gifs-app/src/gifs/actions/get-gifs-by-query.action.ts
+++ gifs-app/src/gifs/actions/get-gifs-by-query.action.ts
@@ -1,19 +1,16 @@
-import axios from "axios";
 import type { GiphyResponse } from "../interfaces/giphy.response";
 import type { Gif } from "../interfaces/gif.interface";
+import { giphyApi } from "../api/giphy.api";
 
 export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
-  const response = await axios.get<GiphyResponse>(
-    "https://api.giphy.com/v1/gifs/search?",
-    {
-      params: {
-        q: query,
-        limit: 10,
-        lang: "es",
-        api_key: "xwRSO0ShtpJEHj5CHYJonaVlIat07VuO",
-      },
+  const response = await giphyApi<GiphyResponse>("/search", {
+    params: {
+      q: query,
+      limit: 10,
+      //lang: "es",
+      //api_key: import.meta.env.VITE_GIPHY_API_KEY,
     },
-  );
+  });
   return response.data.data.map((gif) => ({
     id: gif.id,
     title: gif.title,
