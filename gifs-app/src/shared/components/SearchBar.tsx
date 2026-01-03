5fbe10c1d2314c9f0aff669b51c26fee4472be8f
Andrew Jeremiah Herrrada Villarroel <andrew.herrada.villarroel@gmail.com>
2026-01-03 14:35:23 -0400

    Add useEffect

--- gifs-app/src/shared/components/SearchBar.tsx
+++ gifs-app/src/shared/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState, type KeyboardEvent } from "react";
+import { useEffect, useState, type KeyboardEvent } from "react";
 
 interface Props {
   placeholder?: string;
@@ -6,6 +6,17 @@ interface Props {
 }
 export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onQuery(query);
+    }, 700);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [query, onQuery]);
+
   const handleSearch = () => {
     onQuery(query);
     //setQuery(""); // Limpiar la caja de texto al momento de buscar
